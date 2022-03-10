84d68e5741b8162f81523e59c566e17633c93ce4
Jona Ohme <ohmejona@gmail.com>
2022-03-10 03:38:54 +0100

    added .gitignore & first lexer.c code

--- lexer.c
+++ lexer.c
@@ -7,4 +7,30 @@
 
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
+#include<ctype.h>
 
+int main(int argc, char* argv[]) {
+  
+  // 
+  // reading in the code that is going to be broken down into tokens
+  //  
+
+  FILE *compileDatei;
+  int len = 4096;
+  char buffer[len];
+
+  if (argc != 2) {
+    printf("USAGE: %s <file>\n", argv[0]);
+    return 0;
+  }
+
+  compileDatei = fopen(argv[1], "r");
+  fread(buffer, len+1, 1, compileDatei);
+  printf("%s", buffer);
+  fclose(compileDatei);
+    
+  
+
+  return 0;
+}
